92d264a358caeca52c3b0987ebc3a0ce867586b0
Sam <ArcticEcho@users.noreply.github.com>
2018-02-01 13:43:54 +0000

    Add initial source files

---
+++ Reports/Reports/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reports.Controllers
+{
+    public class ReportController : Controller
+    {
+		[Route("/Report")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Reports/Reports/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Reports
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.Sources.Clear();
+                    config.AddJsonFile("config.json", false, true);
+                })
+                .Build();
+    }
+}
