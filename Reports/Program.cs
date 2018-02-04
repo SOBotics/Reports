46b545bad7c777ceb0a7ee575a5a233ab91f905c
Sam <ArcticEcho@users.noreply.github.com>
2018-02-04 15:23:18 +0000

    Make server listening endpoint configurable

--- Reports/Program.cs
+++ Reports/Program.cs
@@ -1,24 +1,31 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
 namespace Reports
 {
 	public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureAppConfiguration((hostContext, config) =>
-                {
-                    config.Sources.Clear();
-                    config.AddJsonFile("config.json", false, true);
-                })
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseConfiguration(
+					new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddJsonFile("hosting.json", false)
+					.Build()
+				)
+				.ConfigureAppConfiguration((hostContext, config) =>
+				{
+					config.Sources.Clear();
+					config.AddJsonFile("config.json", false, true);
+				})
+				.Build();
+	}
 }
